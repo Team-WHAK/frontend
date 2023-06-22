e87ae601093962e28a98a07c5c3fa452531f0985
wabowen89 <wabowen89@gmail.com>
2023-06-22 13:39:10 -0700

    Added scaffolding code to all pages and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Edit from "./pages/Edit"
+import Home from "./pages/Home"
+import Index from "./pages/IndexPage"
+import LogIn from "./pages/LogIn"
+import New from "./pages/New"
+import NotFound from "./pages/NotFound"
+import ProtectedIndex from "./pages/ProtectedIndex"
+import Show from "./pages/Show"
+import SignUp from "./pages/SignUp"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Edit />
+      <Home />
+      <Index />
+      <LogIn />
+      <New />
+      <NotFound />
+      <ProtectedIndex />
+      <Show />
+      <SignUp />
+      <Footer />
+    </>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+const Footer = () => {
+
+    return (
+        <>
+            <h1>Footer</h1>
+        </>
+    )
+}
+
+export default Footer
