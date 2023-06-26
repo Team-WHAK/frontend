52247b37b7d198534a1957b4c605453874505833
kyloh1996 <kyloh1996@gmail.com>
2023-06-26 13:01:24 -0500

    indentation for react-home

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -10,16 +10,16 @@ const NotFound = () => {
   };
   return (
     <div id='container'>
-    <div className='txt'>
-    <h1>Honey, you've stumbled onto a page that does not exist. But don't buzz off just yet! Come back to the hive, and join the colony. </h1>
-    </div>
-    <div className='returnBtn'>
-    <button style={{backgroundColor: '#992509', height:'6vw', width:'20vw', borderRadius:'2vw', color: '#ffffff',}} onClick={handleReturnHome}>Return to the Hive</button>
-    </div>
-    <div className='cutePics'>
-    <img src='../images/beeHive.jpeg' alt='bee' className="pic1"/>
-    <img src='../images/bee1.jpeg' alt='bee' className='pic2' />
-    </div>
+      <div className='txt'>
+        <h1>Honey, you've stumbled onto a page that does not exist. But don't buzz off just yet! Come back to the hive, and join the colony. </h1>
+      </div>
+      <div className='returnBtn'>
+        <button style={{ backgroundColor: '#992509', height: '6vw', width: '20vw', borderRadius: '2vw', color: '#ffffff', }} onClick={handleReturnHome}>Return to the Hive</button>
+      </div>
+      <div className='cutePics'>
+        <img src='../images/beeHive.jpeg' alt='bee' className="pic1" />
+        <img src='../images/bee1.jpeg' alt='bee' className='pic2' />
+      </div>
     </div>
   );
 };
