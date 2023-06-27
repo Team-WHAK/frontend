fbbcc5f6fd199e355c63d2b3c7dd3e6c2a28306f
kyloh1996 <kyloh1996@gmail.com>
2023-06-27 15:41:15 -0500

    spacing

--- src/_tests_/AboutUs.test.js
+++ src/_tests_/AboutUs.test.js
@@ -5,17 +5,17 @@ import AboutUs from '../pages/AboutUs';
 describe('<AboutUs />', () => {
   it('renders bio section with names and paragraphs', () => {
     render(<AboutUs />);
-    
+
     // Check if names are rendered
     const nameKyle = screen.getByText('Kyle');
     const nameAubrey = screen.getByText('Aubrey');
     const nameWill = screen.getByText('Will');
     const nameHalie = screen.getByText('Halie');
-    
+
     expect(nameKyle).toBeInTheDocument();
     expect(nameAubrey).toBeInTheDocument();
     expect(nameWill).toBeInTheDocument();
     expect(nameHalie).toBeInTheDocument();
-    
+
   });
 });
