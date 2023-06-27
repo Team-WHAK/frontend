e7b63a7013a32740566409ceed3c1d4efcb2e4ce
haliespaige <halie.romasco@gmail.com>
2023-06-27 12:06:04 -0700

    completed tests for Log In and Sign Up pages

--- src/_tests_/LogIn.test.js
+++ src/_tests_/LogIn.test.js
@@ -1,7 +1,22 @@
-import LogIn from "../pages/LogIn";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import LogIn from '../pages/LogIn';
 
-describe("<LogIn />", () => {
-  it("", () => {});
+describe('LogIn', () => {
+  it('should render the login form', () => {
+    render(
+      <BrowserRouter>
+        <LogIn />
+      </BrowserRouter>
+    );
 
-  expect(element).toBeInTheDocument();
-});
+    const emailInput = screen.getByLabelText('Email:');
+    const passwordInput = screen.getByLabelText('Password:');
+    const loginButton = screen.getByRole('button', { name: 'Login' });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+}) 
--- src/_tests_/SignUp.test.js
+++ src/_tests_/SignUp.test.js
@@ -1,7 +1,24 @@
-import SignUp from "../pages/SignUp";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import SignUp from '../pages/SignUp';
 
-describe("<SignUp />", () => {
-  it("", () => {});
+describe('SignUp', () => {
+  it('should render the signup form', () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    );
 
-  expect(element).toBeInTheDocument();
-});
+    const emailInput = screen.getByLabelText('Email:');
+    const passwordInput = screen.getByLabelText('Password:');
+    const confirmPasswordInput = screen.getByLabelText('Confirm Password:');
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(confirmPasswordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+}) 
