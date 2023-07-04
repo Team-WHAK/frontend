0417fec74d2b7cdad9acf8f2489e5c702cba1fff
wabowen89 <wabowen89@gmail.com>
2023-07-03 19:50:43 -0700

    added additional testing to header component

--- src/_tests_/Header.test.js
+++ src/_tests_/Header.test.js
@@ -1,15 +1,81 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 import Header from "../components/Header";
 
 describe("<Header />", () => {
-  it("has an image and attributes assigned to it", () => {
+
+  beforeEach(() => {
     render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>
     )
+  })
+
+  it("has an image and attributes assigned to it", () => {
     const imgAlt = screen.getByAltText(/Logo/i);
     expect(imgAlt).toHaveAttribute("alt", "Logo");
   })
+
+  it("has an about us link", () => {
+    const aboutUsLink = screen.getByText("About Us")
+    expect(aboutUsLink).toBeInTheDocument()
+  })
+
+  it("has a contact link", () => {
+    const contactLink = screen.getByText("Contact")
+    expect(contactLink).toBeInTheDocument()
+  })
+})
+
+describe("<Header /> conditional rendering", () => {
+
+  it("renders home link for authenticated users", () => {
+    render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Header currentUser={true} />} />
+        </Routes>
+      </MemoryRouter>
+    )
+    const homeLink = screen.getByText("Home")
+    expect(homeLink).toBeInTheDocument()
+  })
+
+  it("renders logout link for authenticated users", () => {
+    render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Header currentUser={true} />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const logOutLink = screen.getByText("Log Out");
+    expect(logOutLink).toBeInTheDocument();
+  });
+
+  it("renders sign up link for unauthenticated users", () => {
+    render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Header currentUser={false} />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const signUpLink = screen.getByText("Sign Up");
+    expect(signUpLink).toBeInTheDocument();
+  });
+
+  it("renders login link for unauthenticated users", () => {
+    render(
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Header currentUser={false} />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const logInLink = screen.getByText("Log In");
+    expect(logInLink).toBeInTheDocument();
+  });
+
 })
